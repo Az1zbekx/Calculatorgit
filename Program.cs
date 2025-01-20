496fb62dd236ab74fe5294efcf16c3f32c963f71
Az1zbekx <kalilinux42331@gmail.com>
2025-01-20 11:49:43 +0500

    Calculator code

--- Program.cs
+++ Program.cs
@@ -2,7 +2,12 @@
 namespace Pro{
     class Program{
         static void Main(){
-            Console.WriteLine("Git hub");
+            Console.Write("Enter the numbers to calculate: ");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            decimal a = decimal.Parse(numbers[0]);
+            decimal b = decimal.Parse(numbers[1]);
+            Console.WriteLine("Sum: "+(a+b));
         }
     }
 }
